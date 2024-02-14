ec6cf599e6cc2e8e3446d7ed37fefea0aef974a4
Who am I? <82019.is@gmail.com>
2024-02-14 21:14:40 +0300

    Написано начало основного файла main_menu.py и самый класс display в файле wc.py (worky class). А также прописано взаимодействие main_menu.py и wc.py

--- main_menu.py
+++ main_menu.py
@@ -1,1 +1,8 @@
-import tkinter
+from tkinter import *
+import wc
+
+"""
+Basic functions application Worky
+"""
+worky_display = wc.Display()
+worky_display.window.mainloop()
---
+++ wc.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from PIL import Image, ImageDraw, ImageFont, ImageTk
+
+"""
+This is class for worky
+"""
+
+class Display():
+    """
+    This is basic class of Worky, which displays Worky's Display
+    """
+    window = Tk()
+    window.title('Воркометр 2.5')
+    window.geometry('372x180')
+    foneimage = ImageTk.PhotoImage\
+        (Image.open('d:\Python\Desktop\worky_v_2_5\emplate\media\image\worky.jpg'))
+    fonelabel = Label(window, image=foneimage)
+    fonelabel.place(x=0, y=0)
+    textodometr = '123456'
+    textdistance = '123'
+    textdistancefloat = '4'
+    textlogin = 'None'
+    loginlabel = Label(fonelabel, text=textlogin, font=('Roboto Bold', 12))
+    loginlabel.place(x=5, y=100)
+    odometrlabel = Label(fonelabel, text=textodometr, font=('Roboto Bold', 13))
+    odometrlabel.place(x=156, y=108)
+    distancelabel = Label(fonelabel, text=textdistance, font=('Roboto Bold', 12))
+    distancelabel.place(x=166, y=67)
+    distancefloatlabel = Label(fonelabel, text=textdistancefloat, font=('Roboto Bold', 12))
+    distancefloatlabel.place(x=195, y=67)
+    distancefloatlabel.configure(fg='red')
+    indication = Label(fonelabel, text='Stop', fg='red', bg='black', font=('Roboto Bold', 20))
+    indication.place(x=3, y=130)
+    status = 1
+    generalframe = Frame(master=window, bg='limegreen')
+    generalframe.pack()
