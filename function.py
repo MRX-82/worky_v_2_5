bf492d7ccbd163e076fa9219383581c9f6024ae6
Who am I? <82019.is@gmail.com>
2024-04-12 13:55:37 +0300

    Написана функция преобразовывающая числовое значение в текстовую строку и добавляющая недостающие нули для корректного отображения на дисплее.

---
+++ function.py
@@ -0,0 +1,17 @@
+"""
+This is small function for working aplication "worky"
+"""
+
+def integer_to_string(counter, number):
+    """
+    This function do it translate integer to string for watch to display
+    """
+    counter = str(counter)
+    testing_counter = len(counter)
+    if testing_counter < number:
+        nead_number = number - testing_counter
+        new_counter = '0'*nead_number+counter
+        return new_counter
+    elif testing_counter == number:
+        new_counter = counter
+        return new_counter
--- main_menu.py
+++ main_menu.py
@@ -9,11 +9,11 @@ Basic functions application Worky
 Login = 'Shock'
 counter_dst = 12345
 counter_odo = 12345678
-worky = wdb.Counter(Login, counter_dst, counter_odo)
-display_data = worky.download()
-Login = display_data[0][0]
-counter_dst = display_data[0][1]
-counter_odo = display_data[0][2]
+#worky = wdb.Counter(Login, counter_dst, counter_odo)
+#display_data = worky.download()
+#Login = display_data[0][0]
+#counter_dst = display_data[0][1]
+#counter_odo = display_data[0][2]
 worky_display = wc.Display()
 button_aplication = wc.Buttons()
 button_aplication.button_start()
